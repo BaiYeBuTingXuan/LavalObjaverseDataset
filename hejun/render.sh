702bc328294c1badf7585103297c9e16bdd9f656
BaiYeBuTingXuan <1966573351@qq.com>
2026-02-04 01:23:31 +0800

    remove hejun folder which is useless

--- hejun/render.sh
+++
@@ -1,3 +0,0 @@
-python render.py --split training --upload --upload_retries 5
-python render.py --split testing --upload --upload_retries 5
-python render.py --split validation --upload --upload_retries 5
--- hejun/setup.sh
+++
@@ -1,2 +0,0 @@
-/home/user/Documents/blender-4.3.2-linux-x64/4.3/python/bin/python3.11 -m \
-    pip install torch torchvision torchaudio kornia pillow tqdm opencv-python objaverse tyro --extra-index-url https://download.pytorch.org/whl/cu114

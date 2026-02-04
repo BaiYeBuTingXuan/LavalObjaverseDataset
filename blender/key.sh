5163c2a0cf459b39c1be287284238e9c74a45e29
BaiYeBuTingXuan <1966573351@qq.com>
2026-02-04 18:07:00 +0800

    modify key.sh to generate key easily

--- blender/key.sh
+++ blender/key.sh
@@ -1,12 +1,13 @@
 # 1. Generate SSH key (if you don't have one)
-ssh-keygen -t ed25519 -C "-renderer@$(hostname)" -f ~/.ssh/objaverse_render
+ssh-keygen -t ed25519 -C "-renderer@$(hostname)" -f ~/.ssh/laval-objaverse.pub
 
 # 2. Copy public key to remote server
-ssh-copy-id -i ~/.ssh/objaverse_render.pub user@192.168.1.100
+ssh-copy-id -i ~/.ssh/laval-objaverse.pub hejun@10.21.4.23
 
+mv ~/.ssh/laval-objaverse.pub pwd/
 # 3. Update credentials.json to use key path instead of password:
 {
-  "remote_ip": "192.168.1.100",
+  "remote_ip": "10.21.4.23",
   "remote_user": "render_user",
   "remote_key": "/home/hejun/.ssh/objaverse_render",
   "remote_path": "/data/objaverse_renders"
